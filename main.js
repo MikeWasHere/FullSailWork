6d64ca90d658ed7f86ada8aeff7defe391c1e154
MikeWasHere <xtrjago@gmail.com>
2013-07-17 09:40:04 -0400

    Functions
    This is me doing basic work on functions

---
+++ main.js
@@ -0,0 +1,53 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: MikeWasHere
+ * Date: 7/15/13
+ * Time: 9:10 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+//var grade=92;
+
+//if (grade>89) {console.log("you got an A!!");}
+
+//else if (grade>80){console.log("you got a B!!")}
+
+//else if (grade>70){console.log('you got a C!!')}
+
+//else {console.log("you suck");}
+
+//var hungry = true;
+
+//if (hungry==true){console.log("then eat");}
+
+ // var n = Math.floor(math.random()*5+3)
+
+//var result = (minutes<10 ? "0":"")+minutes+":"+ (seconds<10 ? "0":"")
+//console.log(mm+":"+ss);
+//alert(result)
+
+// var Birthyear = 1990;
+// var age = Birthyear - 2013 +1;
+
+console.log('----Function----');
+
+function helloMsg(){
+    console.log("Hello World!")
+}
+helloMsg()
+function yourName(){
+    console.log("Michael Perez")
+}
+       yourName()
+function Igotthis(){
+    console.log("so basically i do this... words will come up on my console, but my question is, whats the whole point of this?")
+}
+Igotthis()
+
+console.log('----Scope----');
+
+console.log('----Arguments and Parameters----');
+
+console.log('----Return Function----');
+
+console.log('----Annonymous Function----');
