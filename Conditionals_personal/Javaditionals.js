4c6649debf0e9649b05a3e94fc72685214171914
MikeWasHere <xtrjago@gmail.com>
2013-07-26 13:52:45 -0400

    Update_industry
    Updated version of my industry project to fix some of the bugs in it

--- Conditionals_personal/Javaditionals.js
+++ Conditionals_personal/Javaditionals.js
@@ -9,27 +9,23 @@
    function ConditionsInLife (){
     var weather = prompt ("hows it looking outside (use words like 'sunny, raining, cloudy, snowing")
     var sunny = "Go to the beach with the homies"
-    var Raining = "stay in, order some takeout, and watch a movie with the misses"
+    var raining = "stay in, order some takeout, and watch a movie with the misses"
     var cloudy = "Go out to eat with the homies..and maybe the misses, or not."
     var snowing = "Find the tallest hill, crab a trash bin, and let the games begin"
-    if (sunny)
+
+    if (weather === "sunny"){
         console.log (sunny);
-    else ("")
-        console.log
-    prompt("you didn't put anything my guy")
+}else if (weather === "raining"){
+    console.log (raining);
 
-    if (Raining)
-    console.log (Raining);
-
-    if (cloudy)
+    }else if (weather === "cloudy"){
     console.log (cloudy);
 
-    if (snowing)
+    }else if (weather === "snowing"){
     console.log (snowing);
 
-
-
-
-
-        }
-ConditionsInLife()
+}else{
+    prompt("you didn't put anything my guy");
+    }
+}
+ConditionsInLife();
