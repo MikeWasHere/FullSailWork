8d8a1589fb983bba11bccd306428c51ca465bcf6
MikeWasHere <xtrjago@gmail.com>
2013-07-26 14:53:46 -0400

    update_Wacky
    updated version of my wacky code, now allows you to see what you
    actually put

--- Conditionals_Wacky/wackywack.js
+++ Conditionals_Wacky/wackywack.js
@@ -8,7 +8,9 @@
 
 
 var nowthis = prompt ("Now this is the story all about how")
+console.log (nowthis)
     console.log ("My life got flipped, turned upside down ")
 var nowthis2 = prompt ("And I'd like to take a minute just sit right there")
+console.log (nowthis2)
     console.log ("I'll tell you how I became the prince of a town called Bel-air")
 
